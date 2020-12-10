62ad3a23bed874d0c1599e988d7d74ac1e180081
Lomi5 <72826954+Lomi5@users.noreply.github.com>
2020-12-09 23:02:02 -0500

    Add files via upload

---
+++ codigo_fuente/Webscrapping.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+def manitas_a_la_obra(texto_limpio):
+	f = open("frases_inicio.txt","w") #w: write
+	for linea in texto_limpio:
+		f.write(linea)
+
+def fraccionar(texto):
+	lineas = [linea for linea in texto.split('\n') if linea != '']
+	lineas = lineas[87:111]
+	texto_limpio = '\n'.join(lineas)
+	print(texto_limpio)
+	manitas_a_la_obra(texto_limpio)
+
+def main():
+	global html
+
+	url = "https://www.fundacionaquae.org/25-curiosidades-sobre-las-especies-marinas/"
+	page = urlopen(url)
+	html = page.read().decode("utf-8")
+	soup = BeautifulSoup(html, "html.parser")
+	texto = soup.get_text()
+	fraccionar(texto)
+
+main()
