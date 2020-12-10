7719c543fc84384451fe59211ffbc48ab4549669
Lomi5 <72826954+Lomi5@users.noreply.github.com>
2020-12-09 23:06:48 -0500

    Update Webscrapping.py

--- codigo_fuente/Webscrapping.py
+++ codigo_fuente/Webscrapping.py
@@ -8,7 +8,7 @@ def manitas_a_la_obra(texto_limpio):
 
 def fraccionar(texto):
 	lineas = [linea for linea in texto.split('\n') if linea != '']
-	lineas = lineas[87:111]
+	lineas = lineas[87:111]                                                               #El numero se obtuvo de la posicion en el html
 	texto_limpio = '\n'.join(lineas)
 	print(texto_limpio)
 	manitas_a_la_obra(texto_limpio)
@@ -16,7 +16,7 @@ def fraccionar(texto):
 def main():
 	global html
 
-	url = "https://www.fundacionaquae.org/25-curiosidades-sobre-las-especies-marinas/"
+	url = "https://www.fundacionaquae.org/25-curiosidades-sobre-las-especies-marinas/"    #En caso de no funcionar se posee un respaldo
 	page = urlopen(url)
 	html = page.read().decode("utf-8")
 	soup = BeautifulSoup(html, "html.parser")
